a5a1c078cc01e26abf0f54b0dd1953f4f74517db
soloexcel <solomonlekan1@gmail.com>
2023-03-07 06:16:31 -0500

    updated the status component

--- components/Status/Status.jsx
+++ components/Status/Status.jsx
@@ -1,20 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext,useEffect,useState } from 'react'
 import { FaUserAlt } from "react-icons/fa"
 import Style from './Status.module.css'
 import { ElectionContext } from '../../context/Context'
 
-const Status = () => {
 
-  const { walletAddr, actualVoters } = useContext(ElectionContext)
-   return (
+const Status = () => {
+  const { walletAddr, voters, actualVoters } = useContext(ElectionContext);
+  const [status, setStatus] = useState("Yet to Vote");
+
+  useEffect(() => {
+    voters();
+  }, [voters]);
+
+  useEffect(() => {
+    actualVoters.forEach((item) => {
+      if (item.voteraddress.toLowerCase() === walletAddr.toLowerCase()) {
+        item.hasVoted && setStatus("Voted");
+      }
+    });
+  }, [actualVoters]);
+
+  return (
     <div className={Style.status}>
-      <FaUserAlt className={Style.userIcon}/>
+      <FaUserAlt className={Style.userIcon} />
       <p>
-        <span>Wallet Address: {`${walletAddr.substring(0, 7)}...${walletAddr.substring(35, 42)}`}</span>
-        <span>Status: {actualVoters.hasVoted ? "Voted" : "Yet to Vote"}</span>
+        <span>
+          Wallet Address:{" "}
+          {`${walletAddr.substring(0, 7)}...${walletAddr.substring(35, 42)}`}
+        </span>
+        <span>Status: {status}</span>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Status
+export default Status;
+
